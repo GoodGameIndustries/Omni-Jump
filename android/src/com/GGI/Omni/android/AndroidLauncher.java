0528e9cc4b1b493a261bf5f5fe8a11d917f39a81
EmmettGGI <goodgameindustries@gmail.com>
2014-10-05 16:43:56 -0400

    libgdx setup

---
+++ android/src/com/GGI/Omni/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.GGI.Omni.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.GGI.Omni.Omni;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Omni(), config);
+	}
+}
